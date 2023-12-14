ede2c079f65c1cd857f19971e4dd90075ed48dd4
Max <12960866+maxchapin@users.noreply.github.com>
2023-12-13 21:03:34 -0500

    add buy now page

---
+++ src/components/CaseBuyButton.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+function CaseBuyButton() {
+  // Paste the stripe-buy-button snippet in your React component
+  return (
+    <stripe-buy-button
+        buy-button-id="buy_btn_1ON3NFLR4ke7jgCCuFWTcpMa"
+        publishable-key="pk_test_51OLyiuLR4ke7jgCCBHRyokdV1WV1NoMGGkok5zJDc7KL9Y1GYsQXR4aBbpkAXVQkjbBLGiBLDe2NUNIBNvENR65300urYesPHb"
+    >
+    </stripe-buy-button>
+  );
+}
+
+export default CaseBuyButton;
--- src/components/MeetMax.jsx
+++ src/components/MeetMax.jsx
@@ -5,7 +5,7 @@ function MeetMax() {
     return (
         <div className="meet-max">
             <Link to='./About'>
-                <button >Meet Max...</button>
+                <button >Meet Max</button>
             </Link>
             <p>AUVI-Q® and AUVI-q® are registered trademarks of kaleo, Inc. Other referenced trademarks are owned by their respective third-party owners.</p>
         </div>
